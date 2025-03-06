98960fd882ac1ec39b63a5ce5452da529bb37d51
Mikazuki Tomoko <142188839+Yesord@users.noreply.github.com>
2025-03-06 15:18:22 +0800

    add loading animation and preload styles for music subsite

---
+++ static/subsite/music/js/preload.js
@@ -0,0 +1,13 @@
+// document.addEventListener('DOMContentLoaded', function () {
+    
+    
+
+// });
+
+
+var pageLoading = document.querySelector("#music-loading");
+window.addEventListener('load', function() {
+    setTimeout(function () {
+        pageLoading.style.opacity = '0';
+    }, 100);
+});
